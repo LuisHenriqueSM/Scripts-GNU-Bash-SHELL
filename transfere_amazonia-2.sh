cf615d0775d0a1e4e2409c01a59ddf68c37ad1fe
Luis Henrique de Siqueira Martins <93727463+LuisHenriqueSM@users.noreply.github.com>
2021-12-09 11:50:21 -0400

    Add files via upload

--- transfere_amazonia-2.sh
+++ transfere_amazonia-2.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 ########################################################################################
 # Script que realiza a conversao de dados no formato DRD_CBERS-4 para DRD_AMZ1 via ms3 #
@@ -6,13 +6,14 @@
 # Data: 18/03/2021                                                                     #
 ########################################################################################
 # Define variaveis locais #
-path_dir='/amz'
-path_exe='/amz/DADOS-AMAZONIA-1-ENVIADOS'
+path_local='/mnt/sir18/disco1/A1'
+path_dir='/amz2'
+path_exe=$path_local'/DADOS-AMAZONIA-1-ENVIADOS'
 antena='CB11'
-#ingestora='SIR15'
+#ingestora='SIR1'
 passagem=$path_dir'/'$1 
 
-list_dir=`ls -1 $path_dir | grep AWFI`
+list_dir=`ls -1 $path_dir | grep TM`
  
 # Verifica se foram informados todos os parametros #
 
@@ -27,34 +28,35 @@ then
 
                 echo -e "
             Selecione uma das passagens acima e 
-            Execute o comando conforme o exemplo abaixo: \033[0;31m
-                transfere_amazonia-1.sh AWFI-2021-03-17-14-23-31 \033[0m" 
+            Execute o comando conforme o exemplo abaixo (scriot + nome_da_pasta): \033[0;31m
+                transfere_amazonia-2.sh TM-2021-04-30-12-27-43 \033[0m" 
 
 else
 	if [ -d $passagem  ]
 	then
 		cd $passagem
-		CB4_DRD=`ls -1 CBERS_4_*DRD*`		 
-		data_hora=`echo $CB4_DRD |cut -d '_' -f5- `
+		TM_DRD=`ls -1 LANDSAT_5_*DRD*`		 
+		data_hora=`echo $TM_DRD |cut -d '_' -f5- `
+		echo "copiando dado para SIR-18"
+		cp -R $TM_DRD $path_local                
+                
 
 		# Definindo o nome dos arquivos
 		AMZ_RAW='AMAZONIA_1_WFI_RAW_'$data_hora'_'$antena
 		AMZ_DRD='AMAZONIA_1_WFI_DRD_'$data_hora'_'$antena
 		
+		cd $path_local
 		echo " Convertendo para RAW"
-	 	station_drd_to_raw $CB4_DRD $path_exe/$AMZ_RAW	
+	 	station_drd_to_raw $TM_DRD $path_exe/$AMZ_RAW	
 				
 		echo " Convertendo para DRD"
-		station_raw_to_drd  $path_exe/$AMZ_RAW AMAZONIA 1 WFI $path_exe/$AMZ_DRD
+		station_raw_to_drd  $path_exe/$AMZ_RAW AMAZONIA 1 WFI  $path_exe/$AMZ_DRD
                 
                 rm -rf $path_exe/$AMZ_RAW
 		 
                 cd $path_exe
+		echo " O dado sera enviado!"
                 transfere_local.sh $AMZ_DRD
 	fi
 fi
-
-
 exit
-
-

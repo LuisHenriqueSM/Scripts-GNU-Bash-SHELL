50ca6f72bbeac1b27ffd09b0161e914aec9df981
Luis Henrique de Siqueira Martins <93727463+LuisHenriqueSM@users.noreply.github.com>
2021-12-20 07:32:04 -0400

    Update bcktransfere_storage.sh

--- bcktransfere_storage.sh
+++ bcktransfere_storage.sh
@@ -4,7 +4,7 @@
 # Variaveis usadas pelo BBFTP PARA O STORAGE ERG
 
 
-/home/cbers/ncftp-3.2.5/bin/./ncftpput -R -E -u cbers -p cuiabaerg 10.163.155.233 /mnt/disco1/A1 $1 >> /home/cbers/storage.log
+/home/cbers/ncftp-3.2.5/bin/./ncftpput -R -E -u user -p passwd 10.163.155.233 /mnt/disco1/A1 $1 >> /home/cbers/storage.log
 
 
 echo $1 >> /home/cbers/storage.log
